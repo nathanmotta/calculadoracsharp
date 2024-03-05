d6bc1baa23449cbaafe36aea458abfe59d5c2a0a
nathanmotta <142407439+nathanmotta@users.noreply.github.com>
2024-03-04 23:50:47 -0300

    Add files via upload

--- exerciciounisanta/exerciciounisanta/Program.cs
+++ exerciciounisanta/exerciciounisanta/Program.cs
@@ -5,23 +5,31 @@ public class HelloWorld
     public static void Main(string[] args)
     {
         string nome, genero;
-        int salario, idade;
+        int idade, salario;
         float altura;
 
-        Console.WriteLine("Digite seu nome: ");
+        Console.WriteLine("Digite um nome: ");
         nome = Console.ReadLine();
 
-        Console.WriteLine("Digite sua idade: ");
+        Console.WriteLine("Digite um gênero (F/M): ");
+        genero = Console.ReadLine();
+
+        Console.WriteLine("Digite uma idade: ");
         idade = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Digite seu salário: ");
+        Console.WriteLine("Digite um salário: ");
         salario = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Digite sua altura: ");
+        Console.WriteLine("Digite uma altura: ");
         altura = float.Parse(Console.ReadLine());
 
-        Console.WriteLine("Digite seu Gênero (M/F): ");
-        genero = Console.ReadLine();
+        Console.WriteLine("Resultado: ");
+
+        Console.WriteLine("Nome: " + nome);
+        Console.WriteLine("Genero: " + genero);
+        Console.WriteLine("Idade: " + idade);
+        Console.WriteLine("Salário: " + salario);
+        Console.WriteLine("Altura: " + altura);
 
         Console.ReadKey();
     }
